a5b4e015977a75016ec3f11b98def822b98b9311
DavJonesL <zulluhgaming@gmail.com>
2018-04-11 11:51:20 +0100

    23

--- netlifyFunctions/getReg01.js
+++ netlifyFunctions/getReg01.js
@@ -1,6 +1,4 @@
 <script type="text/javascript" src="https://cdn.rawgit.com/chrisveness/crypto/master/sha1.js"></script>
 exports.handler = function(event, context, callback) {
-    callback(null, {
-    body: Sha1.hash("beetlejuice")
-    });
+    callback(null, Sha1.hash("beetlejuice"));
 }
