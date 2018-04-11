1c5bfad0a836d6d3fe3f38a591c7fcb713bc0a2e
DavJonesL <zulluhgaming@gmail.com>
2018-04-11 17:01:44 +0100

    as

--- reg/getReg01.js
+++ reg/getReg01.js
@@ -1,7 +1,9 @@
 function getK(val){
   if (val == "25E85E7D6E8CF97A2E8EA90A04F0E297A54ECEAB") {
-    return 1
+    return 1;
+  } else if (val == "1911FDD636BAF809B67F14E47C1D6F7F24BC9FD9") {
+    return 2;
   } else {
-    return 2
+    return 0;
   }
 }
